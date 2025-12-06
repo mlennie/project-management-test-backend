175e4541404191b274809c13de249b01f6ad4040
Monty Lennie <montylennie@gmail.com>
2025-12-05 21:59:19 -0600

    Fix RSpec test failures by correcting DatabaseCleaner configuration
    Updated rails_helper.rb to use before/after hooks instead of around hook
    for DatabaseCleaner, ensuring test data created with let! is properly
    included in the transaction. Also disabled use_transactional_fixtures
    to avoid conflicts with DatabaseCleaner.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,7 +44,8 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # Disabled because we're using DatabaseCleaner
+  config.use_transactional_fixtures = false
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
@@ -84,18 +85,15 @@ RSpec.configure do |config|
 
   # Database Cleaner configuration
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before(:each, type: :request) do
-    # Allow all hosts for request specs
-    host! "www.example.com"
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 end
